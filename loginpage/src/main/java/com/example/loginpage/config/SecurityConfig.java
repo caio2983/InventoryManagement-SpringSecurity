9710b8a7a0be5f400327bad5782d19bc9cba4926
caio2983 <caioluna2002@gmail.com>
2024-10-18 12:36:23 -0300

    feat: change to postgres DB / create users and product migrations

--- loginpage/src/main/java/com/example/loginpage/config/SecurityConfig.java
+++ loginpage/src/main/java/com/example/loginpage/config/SecurityConfig.java
@@ -1,4 +1,17 @@
 package com.example.loginpage.config;
 
-public class SecurityConfig {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+
+@Configuration
+class securityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
 }
